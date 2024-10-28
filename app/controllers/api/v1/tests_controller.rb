5c2270813412b169e4da35890636469bcee6e5d3
takumimzd <retnuh.takumi@gmail.com>
2024-10-28 22:31:21 +0900

    ユーザーの詳細を返すAPIの作成

--- app/controllers/api/v1/tests_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::TestsController < ApplicationController
-
-  def test
-    render json: { message: "ok" }
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get '/api/v1/tests', to: 'api/v1/tests#test'
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:show]
+    end
+  end
 end
